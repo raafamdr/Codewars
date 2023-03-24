5110d8469b3a97c86931c048bf953ef4318d848a
Rafael del Rio <rafaelgdelrio@gmail.com>
2023-03-24 12:39:08 -0300

    add Quarter of the year

---
+++ 8-kyu/Quarter of the year.py
@@ -0,0 +1,9 @@
+def quarter_of(month):
+    if month in [1,2,3]:
+        return 1
+    elif month in [4,5,6]:
+        return 2
+    elif month in [7,8,9]:
+        return 3
+    else:
+        return 4
